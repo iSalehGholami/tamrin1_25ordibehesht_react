ac5a412c62c8a3ea074264268dc8a90bba2d1bd1
Mo. Saleh Gholami <iSalehGholami@gmail.com>
2024-05-14 19:30:00 +0330

    Configuring app component to use header component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </div >
+  );
+}
+
+export default App;
